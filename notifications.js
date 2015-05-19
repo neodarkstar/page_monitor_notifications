240ff84bc0b84cef84ab9ab49e5bcd19502e0613
John Llombart <jllombart@liquidoperations.com>
2015-05-19 13:39:29 -0400

    Added queue implementation;

--- notifications.js
+++ notifications.js
@@ -1,13 +1,51 @@
 var nodemailer = require('nodemailer');
 var MongoClient = require('mongodb').MongoClient;
+var amqp = require('amqplib');
 
 var url = 'mongodb://localhost:27017/page_monitor';
+
+
+subscribe();
+
+function subscribe(){
+
+  MongoClient.connect(url, function(err, db){
+    process.once('SIGINT', function(){ db.close();});
+
+    amqp.connect('amqp://localhost').then(function(conn){
+      process.once('SIGINT', function(){ conn.close();});
+
+      return conn.createChannel().then(function(ch){
+
+        var ok = ch.assertQueue('email', { durable: false });
+
+        ok = ok.then(function(_qok){
+          return ch.consume('email', function(msg){
+
+            var item = JSON.parse(msg.content.toString());
+
+            console.log(" [x] Received '%s'", 'Item: ' + item.item + ' Store: ' + item.store);
+
+            notify(db,item);
+
+          }, {noAck: true});
+        });
+
+        return ok.then(function(_consumeOk){
+          console.log(' [*] Waiting for messages. To exit press CTRL+C');
+        });
+      });
+    }).then(null, console.warn);
+
+  });
+
+}
 
 function notify(db, site){
 
 	var msg = {
 		subject: site.item + ' has Changed!\n',
-		body:'http://' + site.requestOptions.host + site.requestOptions.path
+		body: site.url
 	};
 
 	var transporter = nodemailer.createTransport({
@@ -33,7 +71,7 @@ function notify(db, site){
 
 			};
 
-		console.log('Notified : ' + user.name + ' @' + user.text);
+		console.log('Notified : ' + user.name + ' ' + user.text);
 
 		transporter.sendMail(mailOptions);
 
